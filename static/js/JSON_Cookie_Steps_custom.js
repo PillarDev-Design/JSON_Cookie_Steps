8686456b1fb1499f1f0559ee91941b0e07173bd8
Christopher Nabors <PillarDD@Gmail.com>
2013-03-01 18:28:38 -0500

    Fixing the logic path of steps

--- static/js/JSON_Cookie_Steps_custom.js
+++ static/js/JSON_Cookie_Steps_custom.js
@@ -4,48 +4,153 @@
  * This file contains the custom code for the JSON_Cookie_Steps project       *
 \******************************************************************************/
 
+/***********************************************\
+ * Save_Cookie                                 *
+ * ------------------------------------------- *
+ * cookieData (array of saved data)            *
+ * stepNumber (current step number on save)    *
+ * completedSteps (array of completed step #'s *
+\***********************************************/
+function Save_Cookie(cookieData, stepNumber, completedSteps){
+    var name = $('#cookie_name').val(),
+        cookieArray = {
+            stepOneQuestionOne: null,
+            stepOneQuestionTwo: null,
+            stepOneQuestionThree: null,
+            stepTwoScientificName: null,
+            stepTwoCommonName: null,
+            stepTwoScale: null,
+            stepTwoWeedManagementArea: null,
+            stepTwoConservationTargetImpacted: null,
+            stepTwoProjectAreaNameAndSize: null,
+            stepTwoPropertyOwner: null,
+            stepTwoIPMDATDateAssessed: null,
+            stepTwoAssessors: null,
+            stepTwoReviewers: null
+        };
+    
+    for(var i=0; i<completedSteps.length; i++){
+        if(completedSteps[i] === "1"){
+            cookieArray.stepOneQuestionOne = cookieData.stepOneQuestionOne;
+            cookieArray.stepOneQuestionTwo = cookieData.stepOneQuestionTwo;
+            cookieArray.stepOneQuestionThree = cookieData.stepOneQuestionThree;
+        } else if(completedSteps[i] === "2"){
+            cookieArray.stepTwoScientificName =  cookieData.stepTwoScientificName;
+            cookieArray.stepTwoCommonName = cookieData.stepTwoCommonName;
+            cookieArray.stepTwoScale = cookieData.stepTwoScale;
+            cookieArray.stepTwoWeedManagementArea = cookieData.stepTwoWeedManagementArea;
+            cookieArray.stepTwoConservationTargetImpacted = cookieData.stepTwoConservationTargetImpacted;
+            cookieArray.stepTwoProjectAreaNameAndSize = cookieData.stepTwoProjectAreaNameAndSize;
+            cookieArray.stepTwoPropertyOwner = cookieData.stepTwoPropertyOwner;
+            cookieArray.stepTwoIPMDATDateAssessed = cookieData.stepTwoIPMDATDateAssessed;
+            cookieArray.stepTwoAssessors = cookieData.stepTwoAssessors;
+            cookieArray.stepTwoReviewers = cookieData.stepTwoReviewers;
+        } else if(completedSteps[i] === "3"){
+
+        } else if(completedSteps[i] === "4"){
+
+        } else if(completedSteps[i] === "5"){
+
+        }
+    }
+
+    $.JSONCookie(name, cookieArray, {path: '/'});
+};
+
+/***********************************************\
+ * Check_Available_Steps                       *
+ * ------------------------------------------- *
+ * cookieData (array of saved data)            *
+ * completedSteps (array of completed step #'s *
+ * currentStep (integer)                       *
+\***********************************************/
+function Check_Available_Steps(cookieData, completedSteps, currentStep){
+    if(completedSteps.length === 0){
+        // No Steps Completed, Fresh Page
+        JSON_Cookie_Step_One_Fresh(cookieData, completedSteps, currentStep);
+    } else {
+        // Steps Have Been Completed
+        // Loop Through And Populate
+        if(currentStep === "1"){
+            //JSON_Cookie_Step_One_Load(cookieData, completedSteps, currentStep);
+        } else if(currentStep === "2"){
+            for(var i=0; i<completedSteps.length; i++){
+                if(completedSteps[i] === "2"){
+                    //JSON_Cookie_Step_Two_Fresh(cookieData, completedSteps, currentStep);
+                } else {
+                    //JSON_Cookie_Step_Two_Load(cookieData, completedSteps, currentStep);
+                }
+            }
+        } else if(currentStep === "3"){
+
+        } else if(currentStep === "4"){
+
+        } else if(currentStep === "5"){
+
+        }
+    }
+};
+
+/***********************************************\
+ * JSON_Cookie_Steps_Init                      *
+ * ------------------------------------------- *
+ * Initial function being fired on page        *
+\***********************************************/
 function JSON_Cookie_Steps_Init(){
+    // Create Empty JSON to Pass between Functions
+    var cookieData = {
+        stepOneQuestionOne: null,
+        stepOneQuestionTwo: null,
+        stepOneQuestionThree: null,
+        stepTwoScientificName: null,
+        stepTwoCommonName: null,
+        stepTwoScale: null,
+        stepTwoWeedManagementArea: null,
+        stepTwoConservationTargetImpacted: null,
+        stepTwoProjectAreaNameAndSize: null,
+        stepTwoPropertyOwner: null,
+        stepTwoIPMDATDateAssessed: null,
+        stepTwoAssessors: null,
+        stepTwoReviewers: null
+    },
+        completedSteps = [],
+        currentStep = "1",
+        name = $('#cookie_name').val();
+
     // Assign Functionality to console.log
     $('#console_log').click(function(){
-        var name = $('#cookie_name').val();
         var object = $.JSONCookie(name);
         console.log(object);
     });
+    
+    // Assign Functionality to clear cookie
+    $('#clear_cookie').click(function(){
+        $.JSONCookie(name, cookieData, {path: '/'});
+    });
 
-    // Create Empty JSON to Pass between Functions
-    var cookieData = {
-        stepOneQuestionOne: "",
-        stepOneQuestionTwo: "",
-        stepOneQuestionThree: ""
-    };
+    // Check For Previous Data
+    var savedData = $.JSONCookie(name);
+    if(savedData.stepOneQuestionOne !== null){
+        completedSteps.push("1");
+    }
+    if(savedData.stepTwoScientificName !== null){
+        completedSteps.push("2");
+    }
 
-    /***********************************************\
-     * Step 1                                      *
-    \***********************************************/
-    JSON_Cookie_Step_One(cookieData);
+    // Fire off the initial Step (1)
+    Check_Available_Steps(cookieData, completedSteps, currentStep);
 };
-
-function Save_Cookie(cookieData, stepNumber){
-    var name = $('#cookie_name').val(),
-        cookieArray = {
-            stepOneQuestionOne: cookieData.stepOneQuestionOne,
-            stepOneQuestionTwo: cookieData.stepOneQuestionTwo,
-            stepOneQuestionThree: cookieData.stepOneQuestionThree
-        };
-
-    $.JSONCookie(name, cookieArray, {path: '/'});
-}
 
 /***********************************************\
  * Step 1                                      *
+ * ------------------------------------------- *
+ * cookieData (array of saved data)            *
+ * completedSteps (array of completed step #'s *
+ * currentStep (integer)                       *
 \***********************************************/
-function JSON_Cookie_Step_One(cookieData){
+function JSON_Cookie_Step_One_Fresh(cookieData, completedSteps, currentStep){
     // Declare Variables
-    var obj,
-        name,
-        val,
-        new_val,
-        stepOneQuestionOneAnswer = null,
+    var stepOneQuestionOneAnswer = null,
         stepOneQuestionTwoAnswer = null,
         stepOneQuestionThreeAnswer = null,
         saveArray = {};
@@ -55,9 +160,6 @@ function JSON_Cookie_Step_One(cookieData){
         categoryTwoSubcategories = "<option value='catTwoSubOne'>Category Two - Subcategory One</option><option value='catTwoSubTwo'>Category Two - Subcategory Two</option><option value='catTwoSubThree'>Category Two - Subcategory Three</option>",
         categoryThreeSubcategories = "<option value='catThreeSubOne'>Category Three - Subcategory One</option>";
     
-    // Make the Select Lists Blank
-    $('#content_step_one_question_one').prop('selectedIndex', -1);
-
     // Function to check if form is complete
     function Step_One_Form_Check(q1, q2, q3){
         if((q1 !== null) && (q2 !== null) && (q3 !== null)){
@@ -78,6 +180,9 @@ function JSON_Cookie_Step_One(cookieData){
             } 
         }
     };
+
+    // Make the Select Lists Blank
+    $('#content_step_one_question_one').prop('selectedIndex', -1);
 
     // Assign Events to Content Fields
     $('#content_step_one_question_one').change(function(){
@@ -108,14 +213,6 @@ function JSON_Cookie_Step_One(cookieData){
     
     
     function Check_Boxes(){
-        // Check if All Boxes are unchecked
-        /*
-        if(($('#content_step_one_question_three_box_one').not(':checked')) && ($('#content_step_one_question_three_box_two').not(':checked')) && ($('#content_step_one_question_three_box_three').not(':checked'))){
-            stepOneQuestionThreeAnswer = null; 
-            console.log('NOT CHECKED');
-        }
-        */
-
         if($('#content_step_one_question_three_box_one').is(':checked')){
             stepOneQuestionThreeAnswer = ['none'];
         } else if(($('#content_step_one_question_three_box_two').is(':checked')) && ($('#content_step_one_question_three_box_three').is(':checked'))){
@@ -171,9 +268,138 @@ function JSON_Cookie_Step_One(cookieData){
                 stepOneQuestionTwo: stepOneQuestionTwoAnswer,
                 stepOneQuestionThree: stepOneQuestionThreeAnswer
             };
+            completedSteps.push("1");
+            Save_Cookie(saveArray, "stepOne", completedSteps);
+            //JSON_Cookie_Step_Two(cookieData, completedSteps);
+        } else {
+            // Failure
+        }
+    });
+    
+    // Assign Events to Step Buttons
+    $('#content_progress_bar_one').click(function(){
+        // Clear Event
+    });
+    $('#content_progress_bar_two').click(function(){
+        // Continue to Step Two if Step 1 is completed.
+        if($('#content_nav_forward').hasClass('content_nav_base_active')){
+            // Fire off Save
+            saveArray = {
+                stepOneQuestionOne: stepOneQuestionOneAnswer,
+                stepOneQuestionTwo: stepOneQuestionTwoAnswer,
+                stepOneQuestionThree: stepOneQuestionThreeAnswer
+            };
+            completedSteps.push("1");
+            Save_Cookie(saveArray, "stepOne", completedSteps);
+            //JSON_Cookie_Step_Two(cookieData, completedSteps);
+        } else {
+            // Failure
+        }
+    });
+    $('#content_progress_bar_three').click(function(){
+
+    });
+    $('#content_progress_bar_four').click(function(){
+
+    });
+    $('#content_progress_bar_five').click(function(){
+
+    });
+};
+
+function JSON_Cookie_Step_One_Load(cookieData, completedSteps, currentStep){
+
+};
+
+function JSON_Cookie_Step_One_Loadout(){
+
+};
+
+/***********************************************\
+ * Step 2                                      *
+ * ------------------------------------------- *
+ * cookieData (array of saved data)            *
+ * completedSteps (array of completed step #'s *
+ * currentStep (integer)                       *
+\***********************************************/
+function JSON_Cookie_Step_Two(cookieData, completedSteps){
+    // Declare Variables
+    var stepTwoScientificName = null,
+        stepTwoCommonName = null,
+        stepTwoScale = null,
+        stepTwoWeedManagementArea = null,
+        stepTwoConservationTargetImpacted = null,
+        stepTwoProjectAreaNameAndSize = null,
+        stepTwoPropertyOwner = null,
+        stepTwoIPMDATDateAssessed = null,
+        stepTwoAssessors = null,
+        stepTwoReviewers = null,
+        saveArray = {};
+    
+    // Switch steps
+    for(var i=0; i<completedSteps.length; i++){
+        
+    }
+
+    // Subcategory Values
+    
+    // Make the Select Lists Blank
+
+    // Function to check if form is complete
+    function Step_One_Form_Check(q1, q2, q3){
+        if((q1 !== null) && (q2 !== null) && (q3 !== null)){
+            if($('#content_nav_forward').hasClass('content_nav_base_inactive')){
+                // Forward/Back Buttons
+                $('#content_nav_forward').removeClass('content_nav_base_inactive');
+                $('#content_nav_forward').addClass('content_nav_base_active');
+                // Progress Bar
+                $('#content_progress_bar_two').removeClass('progress_bar_inactive');
+                $('#content_progress_bar_two').addClass('progress_bar_available');
+            }
+        } else if((q1 === null) || (q2 === null) || (q3 === null)){
+            if($('#content_nav_forward').hasClass('content_nav_base_active')){
+                // Forward/Back Buttons
+                $('#content_nav_forward').removeClass('content_nav_base_active');
+                $('#content_nav_forward').addClass('content_nav_base_inactive');
+                // Progress Bar
+                $('#content_progress_bar_two').removeClass('progress_bar_available');
+                $('#content_progress_bar_two').addClass('progress_bar_inactive');
+            } 
+        }
+    };
+
+    // Assign Events to Content Fields
+    $('#content_step_one_question_one').change(function(){
+        // Assign Variable
+        stepOneQuestionOneAnswer = $('#content_step_one_question_one').val();
+        stepOneQuestionTwoAnswer = null;
+
+        // Execute Step_One_Form_Check
+        Step_One_Form_Check(stepOneQuestionOneAnswer, stepOneQuestionTwoAnswer, stepOneQuestionThreeAnswer);
+    });
+
+    // *** Question Three Section ***
+    $('#content_step_one_question_three_box_one').change(function(){
+    });
+    $('#content_step_one_question_three_box_two').change(function(){
+    });
+    $('#content_step_one_question_three_box_three').change(function(){
+    });
+
+    // Assign Events to Next Button
+    $('#content_nav_forward').click(function(){
+        if($('#content_nav_forward').hasClass('content_nav_base_active')){
+            // Fire off Save
+            saveArray = {
+                stepOneQuestionOne: stepOneQuestionOneAnswer,
+                stepOneQuestionTwo: stepOneQuestionTwoAnswer,
+                stepOneQuestionThree: stepOneQuestionThreeAnswer
+            };
             Save_Cookie(saveArray, "stepOne");
         } else {
             // Failure
         }
     });
+    // Assign Events to Back Button
+
 };
